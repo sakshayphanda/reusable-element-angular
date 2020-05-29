508ed3f298c15c23d84c552b2733ca4e9ba418e8
Sakshay Phanda <sakshayphanda03@gmail.com>
2020-05-29 12:13:40 +0530

    fix

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
  // selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.sass']
+  styleUrls: ['./search-bar.component.sass'],
+  encapsulation: ViewEncapsulation.Native,
 })
 export class SearchBarComponent implements OnInit {
 
